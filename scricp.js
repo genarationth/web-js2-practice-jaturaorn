80e5ac8495d304b628e9ebca467059a2233f287d
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-11 05:54:35 +0000

    do assign 2.3, add Obj.

--- scricp.js
+++ scricp.js
@@ -1,12 +1,19 @@
-const birthYear = 1998;
-const futureYear = 2024;
-const age = 25;
-const Futureage = 26;
-console.log(`you born in ${birthYear} , then in ${futureYear}. 
-            You'll be ${age} or ${Futureage}`);
+// const birthYear = 1998;
+// const futureYear = 2024;
+// const age = 25;
+// const Futureage = 26;
+// console.log(`you born in ${birthYear} , then in ${futureYear}. 
+//             You'll be ${age} or ${Futureage}`);
 
 // const d = new Date("July 21, 1983 01:15:00");
 // const year = d.getFullYear();
 // const day = d.getDate()
 // document.getElementById("demo").innerHTML = year;
 // document.getElementById("demo").innerHTML = day;
+
+const Name = 'Taica';
+const Location = 'Poland';
+const Hobby = 'drinking wine'
+//let Account = {Name: 'Hi, my name is Johan', Location: 'I live in Peru', Hobby: 'drink Beer'};
+
+console.log(`Hi my name is ${Name}. I live in ${Location} and I enjoy${Hobby}. `);
